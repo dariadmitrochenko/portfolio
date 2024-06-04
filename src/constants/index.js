020181b0a23583cf7a1b59c5b5b11dc243ec9771
Daria Dmitrochenko <dariadmitrochenko@Evgeny.lan>
2024-06-04 06:51:37 -0400

    created navlinks

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+    {
+      id: "home",
+      title: "Home",
+    },
+    {
+      id: "about",
+      title: "About Me",
+    },
+    {
+      id: "projects",
+      title: "Projects",
+    },
+    {
+      id: "CV",
+      title: "CV",
+    },
+  ];
